a275bf2d1a02531af0c71a051f18f7f4891dc655
Ana Luísa <86081728+almt00@users.noreply.github.com>
2022-09-07 18:49:22 +0100

    home progress

--- scripts/index.js
+++ scripts/index.js
@@ -4,19 +4,20 @@ let toggle_state = nav_toggle.getAttribute("aria-expanded");
 
 nav_toggle.addEventListener("click", () => {
   if (toggle_state === "true") {
-    navbar.style.transform ="translateX(100%)";
+    navbar.style.transform = "translateX(100%)";
     navbar.style.transition = "transform 250ms ease-out, visibility 250ms";
     navbar.style.visibility = "hidden";
     toggle_state = "false";
     nav_toggle.innerHTML = '<i class="fa-solid fa-bars fa-xl"></i>';
   } else if (toggle_state === "false") {
-    navbar.style.visibility = "visible";
-    navbar.style.display = "flex";
-    navbar.style.transform ="translateX(0%)";
-    navbar.style.transition = "transform 250ms ease-in";
+    if (nav_toggle.display != "none") {
+      navbar.style.visibility = "visible";
+      navbar.style.display = "flex";
+      navbar.style.transform = "translateX(0%)";
+      navbar.style.transition = "transform 250ms ease-in";
 
- nav_toggle.innerHTML = '<i class="fa-solid fa-x fa-xl"></i>';
-    toggle_state = "true";
-    
+      nav_toggle.innerHTML = '<i class="fa-solid fa-x fa-xl"></i>';
+      toggle_state = "true";
+    }
   }
 });
